ac4948b4edd8476c266ef501fbfa7df4ec3ca9db
alberba <albertsalomv@gmail.com>
2024-04-18 18:23:19 +0200

    Pantalla-museo1 Responsive

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+let swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+let exposicionSwiper = new Swiper(".exposiciones", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
