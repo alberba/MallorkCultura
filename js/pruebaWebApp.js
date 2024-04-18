9a6b9d097821404857e18766c8f3e3379bed2a65
mponsetim <matias.ponseti1@estudiant.uib.cat>
2024-04-18 10:41:27 +0200

    función para añadir los pueblos restantes

--- js/pruebaWebApp.js
+++ js/pruebaWebApp.js
@@ -1,5 +1,10 @@
 // Rehacer la página principal
 $("#logo").on("click",function() {
+    crearDondeVisitar();
+});
+
+//esta función se encarga de "Crear" el apartado de título "¿Dónde visitar?"
+function crearDondeVisitar() {
     nombres =           ["Palma", "Manacor", "Sóller", "Valldemossa","Inca", "Calvià", "Alcúdia", "Santanyí", "Porreres"];
     nombresSinTilde =   ["Palma", "Manacor", "Soller", "Valldemossa","Inca", "Calvia", "Alcudia", "Santanyi", "Porreres"];
     $("main").empty();
@@ -14,16 +19,19 @@ $("#logo").on("click",function() {
     $("main").append(sec);
     $("main").append(crearBotonVerMas_Pueblos());
     $("main").append(crearHr());
-});
+}
 
+// función genérica
 function crearSection () {
     return $("<section></section>");
 }
 
+//función específica
 function crearContenedorPueblos() {
     return $("<div>").attr("class", "contenedor-pueblos my-5");
 }
 
+// función específica
 function crearBotonPueblo (name, nameST) {
     let nuevoBotonPueblo = $("<a>")
     .attr({
@@ -44,23 +52,27 @@ function crearBotonPueblo (name, nameST) {
     return nuevoBotonPueblo;
 }
 
+// función genérica
 function crearH2(titulo) {
     return $("<h2>").addClass("mt-5").html(titulo);
 }
 
+// función genérica ??
 function crearHr() {
     return $("<hr>");
 }
 
+// función específica
 function añadirPueblosRestantes() {
-    pueblos =           ["Palma","Manacor","Valldemossa","Sóller","Fornalux","Deià","Sant Joan","Banyalbufar","Maria de la Salut","Artà","Porreres","Santa Eugènia","Alcúdia","Inca","Sencelles","Sant Lorenç des Cardassar","Santa Margalida","Petra","Lloseta","Mancor de la Vall","Montuïri","Ses Salines","Santa Maria del Camí","Capdepera","Alaró","Ariany","Bunyola","Estellencs","Costitx","Santanyí","Llucmajor","Pollença","Puigpunyent","Campanet","Felanitx","Algaida","Llubí","Sineu","Búger","Calvià","Esporles","Binissalem","Escorca","Sa Pobla","Andratx","Son Servera","Campos","Marratxí","Consell","Lloret de Vistalegre","Vilafranca de Bonany"];
-    pueblosSinTilde =   ["Palma","Manacor","Valldemossa","Soller","Fornalux","Deia","Sant Joan","Banyalbufar","Maria de la Salut","Arta","Porreres","Santa Eugenia","Alcudia","Inca","Sencelles","Sant Lorenç des Cardassar","Santa Margalida","Petra","Lloseta","Mancor de la Vall","Montuiri","Ses Salines","Santa Maria del Cami","Capdepera","Alaro","Ariany","Bunyola","Estellencs","Costitx","Santanyi","Llucmajor","Pollença","Puigpunyent","Campanet","Felanitx","Algaida","Llubi","Sineu","Buger","Calvia","Esporles","Binissalem","Escorca","Sa Pobla","Andratx","Son Servera","Campos","Marratxi","Consell","Lloret de Vistalegre","Vilafranca de Bonany"];
-    
-    // for(let i = 0; i < 9; i++) {
-    //     $("main section .contenedor-pueblos").append(crearBotonPueblo(nombres[i],nombresSinTilde[i]));
-    // } 
+    pueblos =           ["Fornalux","Deià","Sant Joan","Banyalbufar","Maria de la Salut","Artà","Santa Eugènia","Sencelles","Sant Lorenç des Cardassar","Santa Margalida","Petra","Lloseta","Mancor de la Vall","Montuïri","Ses Salines","Santa Maria del Camí","Capdepera","Alaró","Ariany","Bunyola","Estellencs","Costitx","Llucmajor","Pollença","Puigpunyent","Campanet","Felanitx","Algaida","Llubí","Sineu","Búger","Esporles","Binissalem","Escorca","Sa Pobla","Andratx","Son Servera","Campos","Marratxí","Consell","Lloret de Vistalegre","Vilafranca de Bonany"];
+    pueblosSinTilde =   ["Fornalux","Deia","Sant Joan","Banyalbufar","Maria de la Salut","Arta","Santa Eugenia","Sencelles","Sant Lorenç des Cardassar","Santa Margalida","Petra","Lloseta","Mancor de la Vall","Montuiri","Ses Salines","Santa Maria del Cami","Capdepera","Alaro","Ariany","Bunyola","Estellencs","Costitx","Llucmajor","Pollença","Puigpunyent","Campanet","Felanitx","Algaida","Llubi","Sineu","Buger","Esporles","Binissalem","Escorca","Sa Pobla","Andratx","Son Servera","Campos","Marratxi","Consell","Lloret de Vistalegre","Vilafranca de Bonany"];
+    for(let i = 0; i<pueblos.length; i++) {
+        $("main section .contenedor-pueblos").append(crearBotonPueblo(pueblos[i],pueblosSinTilde[i]));
+    } 
+    $("#verMasPueblos").remove();
 }
 
+// función específica
 function crearBotonVerMas_Pueblos() {
     return $("<button>")
             .addClass("boton boton-vermas-index boton-verde")
