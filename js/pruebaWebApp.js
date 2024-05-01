f2f7bae2a1dd24514a62a3068cb11c9741e9431b
mponsetim <matias.ponseti1@estudiant.uib.cat>
2024-05-01 12:20:53 +0200

    El botón ya no envía el los filtros

--- js/pruebaWebApp.js
+++ js/pruebaWebApp.js
@@ -599,10 +599,11 @@ function crearFiltros() {
             .append(crearImg("img/svg/flecha-menu-down.svg"))
             .attr({
                 "data-bs-toggle":"collapse",
-                "data-bs-target":"#form-filtro"
+                "data-bs-target":"#campos-filtro"
             })
         );
-    let form = crearForm("form-filtro","form-filtro-museos mt-3 collapse show")
+    
+    let camposFiltros = crearDiv("campos-filtro-museos mt-3 collapse show","campos-filtro")
         .append(crearDiv("elemento-filtro-museo")   // Input texto nombre
             .append(crearLabel("busqueda-nombre","Búsqueda por nombre"))
             .append(crearInput("search","nombre","busqueda-nombre","","Buscar museo")
@@ -656,7 +657,7 @@ function crearFiltros() {
                                 .on("click",() => cambiarUbicacionesCercaUsuario())
                             ) 
                 );
-    filtros.append(form);
+    filtros.append(camposFiltros);
     
     return filtros;
 }
